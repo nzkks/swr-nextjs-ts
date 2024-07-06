e5309f2f06e77e262c4ce5a89f397d8e1a558118
nzkks <kkshanthosh@gmail.com>
2024-07-07 09:05:18 +1200

    A way to create new product

--- app/components/Products.tsx
+++ app/components/Products.tsx
@@ -1,8 +1,23 @@
 'use client';
 
+import { ChangeEvent, useState } from 'react';
+
+import { axiosInstance } from '../services/fetcher';
 import { useProducts } from '../services/queries';
 
 const Products = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleUpdateInputValue = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleCreateProduct = async () => {
+    await axiosInstance.post('/products', { name: inputValue });
+
+    setInputValue('');
+  };
+
   const productQuery = useProducts();
 
   if (productQuery.isLoading) return <div>Loading...</div>;
@@ -10,7 +25,13 @@ const Products = () => {
 
   return (
     <div>
-      <p>Products:</p>
+      <div>
+        <h4>Create Products</h4>
+        <input value={inputValue} onChange={handleUpdateInputValue} placeholder="Product name" />
+        <button onClick={handleCreateProduct}>Create</button>
+      </div>
+
+      <p>All Products:</p>
       <ul>
         {productQuery.data?.map(product => (
           <li key={product.id}>{product.name}</li>
