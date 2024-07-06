3f272b32dc8610c4357d62f1f633fac9037d0eb3
nzkks <kkshanthosh@gmail.com>
2024-07-07 10:11:07 +1200

    Products: On triggeringthe mutation, use the rollbackOnError as the api option parameter

--- app/components/Products.tsx
+++ app/components/Products.tsx
@@ -21,6 +21,9 @@ const Products = () => {
         // optimisticData - data to immediately update the client cache, or a function that receives current data and returns the new client cache data, usually used in optimistic UI.
         // Simply, when triggering the mutation, immediately show the new data while creating the new data in the backend and refresh the data in the frontend.
         optimisticData: products && [...products, { name: inputValue }],
+        // rollbackOnError - should the cache rollback if the remote mutation errors, or a function that receives the error thrown from fetcher as arguments and returns a boolean whether should rollback or not.
+        // simply, when the mutation throws an error, rollback the cache. It means we can safely use the previous data (products) in the client cache.
+        rollbackOnError: true,
       }
     );
     setInputValue('');
