36780a4178031e80675e06482c1ad273ab84123e
nzkks <kkshanthosh@gmail.com>
2024-07-06 21:51:54 +1200

    Show user name also inside the cart component

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -1,12 +1,24 @@
 'use client';
 
-import { useCart } from '../services/queries';
+import { useCart, useUser } from '../services/queries';
 
 const Cart = () => {
+  const userQuery = useUser();
   const cartQuery = useCart();
 
   return (
     <div>
+      <div>
+        {userQuery.isLoading ? (
+          'Loading user data...'
+        ) : (
+          <div>
+            User name: <b>{userQuery.data?.userName}</b>
+          </div>
+        )}
+      </div>
+      <br />
+      <div>Cart products:</div>
       {cartQuery.data?.products.map(product => (
         <div key={product}>{product}</div>
       ))}
