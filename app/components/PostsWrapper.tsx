52e9494fbac6a3cbcc6b8676d97f8aef1636e56d
nzkks <kkshanthosh@gmail.com>
2024-07-07 15:03:21 +1200

    Posts: prefetch next page data

--- app/components/PostsWrapper.tsx
+++ app/components/PostsWrapper.tsx
@@ -5,11 +5,15 @@ import Posts from './Posts';
 
 const PostsWrapper = () => {
   const [pageIndex, setPageIndex] = useState(1);
+  console.log('pageIndex', pageIndex);
 
   return (
     <>
       <h3>Posts</h3>
       <Posts pageIndex={pageIndex} />
+      <div style={{ display: 'none' }}>
+        <Posts pageIndex={pageIndex + 1} />
+      </div>
 
       <button onClick={() => setPageIndex(pageIndex - 1)}>Prev</button>
       <button onClick={() => setPageIndex(pageIndex + 1)}>Next</button>
